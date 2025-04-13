6f74259b53415bbedc7c2bea46e72dc1db95d392
Your Name <you@example.com>
2025-04-13 14:27:15 -0400

    Add RefineAA environment script

---
+++ refineaa_base.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# RefineAA Environment Script
+# Copyright (c) 2025 Alex Goven
+# Attribution required to InjectSMAA developer and Alex Goven
+# Commercial use requires 1% revenue share to Alex Goven
+
+echo "RefineAA Environment initialized"
